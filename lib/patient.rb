1211750595818a80e2ae4fce9c4183a43743330e
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-01 04:39:46 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,11 +15,11 @@ class Patient
     Appointment.new(date, self, doctor)
   end
 
-  # def appointments
-  #   Appointment.all.select do |date|
-  #     date.patient == self
-  #   end
-  # end
+  def appointments
+    Appointment.all.select do |date|
+      date.patient == self
+    end
+  end
 
   def doctors
     self.appointment.collect do |doctor|
