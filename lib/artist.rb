dbce88e5cb951e0a59ff77ebcf7096678742d1f4
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-01 00:30:17 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def new_song(song_name, genre)
+    @songs << Song.new(song_name, genre)
+    song.artist = self
+  end
+    
+  
