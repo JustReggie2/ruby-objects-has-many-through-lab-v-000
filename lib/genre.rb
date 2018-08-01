2c18f1e809685bc581a40065c9f29017978b4526
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-01 02:18:03 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,13 +12,19 @@ class Genre
     @@all
   end
 
-  # def new_song(name, artist)
-  #   Song.new(name, artist, self)
-  # end
+  def new_song(name, artist)
+    Song.new(name, artist, self)
+  end
 
-  # def songs
-  #   Song.all.select do |song|
-  #     song.genre == self
-  #   end
-  # end
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    self.songs.collect do |song|
+      song.artist
+    end
+  end
 end
