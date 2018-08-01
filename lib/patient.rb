c6d03a5908d3a2896d204509232cd2530b07e88a
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-01 03:54:11 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,25 @@
+class Patient 
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(doctor, date, self)
+  end
+
+  def appointments
+    Appointment.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    self.appointment.collect do |doctor|
+      appointment.doctor
+    end
+  end
+end
